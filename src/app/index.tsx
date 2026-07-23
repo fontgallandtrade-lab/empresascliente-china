ccda009e35f751e50359597f1ef9a1de17b070c6
tradetatui <tradetatui@gmail.com>
2026-07-23 06:29:43 +0000

    Restaura fluxo do aplicativo e mantém tela de código

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,15 +1,5 @@
 import { Redirect } from 'expo-router';
 
 export default function Index() {
-  return (
-    <Redirect
-      href={{
-        pathname: '/codigo-seguranca',
-        params: {
-          type: 'pickup',
-          publicCode: 'CF260723A82F4C',
-        },
-      }}
-    />
-  );
+  return <Redirect href="/login" />;
 }
